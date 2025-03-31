491792ddc85bc0eaba018e305750ec9f87638669
A <a@localhost.localdomain>
2025-04-01 03:36:10 +0500

    Commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const {heroui} = require('@heroui/theme');
+// tailwind.config.js
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [heroui()],
+}
